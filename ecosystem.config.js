72d6ead3ec5d6065bb84aa83801e24b8563d5fd1
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-09-29 16:35:20 +0200

    added deployment configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   apps: [{
-    name: 'standalone',
+    name: 'casa-corrently-ipfs-edge',
     script: 'standalone.js',
-    max_memory_restart: '320M'
-  }]
+    max_memory_restart: '320M',
+    restart_delay: 3000
+  }],
+  "deploy" : {
+     "production" : {
+       "user" : "corrently",
+       "host" : ["l2.stromdao.de"],
+       "ref"  : "origin/master",
+       "repo" : "https://github.com/energychain/casa-corrently-ipfs-edge.git",
+       "path" : "/opt/casa-corrently/ipfs-edge",
+       "post-deploy" : "npm install; npm ci"
+      },
+   }
 }
