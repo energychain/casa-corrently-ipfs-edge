cf296732008d00e0b47813f17ab872d4e12c6879
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-10-11 16:00:22 +0200

    Auto Commit

--- index.js
+++ index.js
@@ -55,7 +55,10 @@ module.exports = function(config) {
       },
       publish: async function(msg,alias) {
           if(ipfs_service == null) await _ipfs_init(config);
-          ipfs_service.postMessage({"msg":msg,"alias":alias})
+          ipfs_service.postMessage({"msg":msg,"alias":alias});
+          if((typeof msgcids[cid]!== 'undefined') && (typeof msgcids[cid].localHistory !== 'undefined')) {
+              return msgcids[cid].localHistory;
+          } else return [];
       }
     }
 };
